a8030fc1e3f439400cb8da31fe363ac5ca36dc32
Branchy <gusts.zarins@gmail.com>
2026-06-15 01:49:38 +0300

    Fix hall place display

--- resources/views/classes/partials/list.blade.php
+++ resources/views/classes/partials/list.blade.php
@@ -6,7 +6,7 @@
         <p><strong>{{ __('messages.category') }}:</strong> {{ $class->category->name ?? '-' }}</p>
         <p><strong>{{ __('messages.date') }}:</strong> {{ optional($class->schedule)->class_date }} </p>
         <p><strong>{{ __('messages.time') }}:</strong> {{ optional($class->schedule)->start_time }} - {{ optional($class->schedule)->end_time }}</p>
-        <p><strong>{{ __('messages.hall') }}:</strong> {{ optional($class->schedule)->place ?? 'Nav norādīta' }}</p>
+        <p><strong>{{ __('messages.hall') }}:</strong> {{ optional($class->schedule)->place ?? '-' }}</p>
         <p><strong>{{ __('messages.available_places') }}:</strong> {{ $class->schedule->available_places ?? '-' }}</p>
       <a class="btn-secondary btn" href="{{ url('/classes/' . $class->id) }}">{{ __('messages.details') }}</a>
    @auth
